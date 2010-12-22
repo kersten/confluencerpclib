c666bf56e516c8ca71926ab7b3d61f275dc10094
Kersten Burkhardt <kerstenk@gmail.com>
2010-12-22 16:18:08 +0100

    Added license and build files

---
+++ setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+    setup(
+        name = 'confluencerpclib',
+        version = '0.1',
+        url = 'http://github.com/kersten/confluencerpclib',
+        license = 'original BSD license',
+        description = 'A python library to connect to Confluence wiki.',
+        long_description = read('README'),
+
+        author = 'Kersten Burkhardt',
+        author_email = 'kerstenk@gmail.com',
+
+        packages = find_packages('src'),
+        package_dir = {'': 'src'},
+
+        install_requires = ['setuptools'],
+
+        classifiers = [
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: original BSD license',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Topic :: Internet :: WWW/HTTP',
+        ],
+
+        entry_points = {
+            'console_scripts': [
+                'confluence = confluencerpclib.confluence:main'
+            ]
+        }
+    )
